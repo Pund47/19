b0abb74208db7b959fc45d8eda7615eb757bc2ee
Pund47 <ilin_yuriy@mail.ru>
2024-12-26 15:00:47 +0300

    1

--- my_sho/my_sho/settings.py
+++ my_sho/my_sho/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'my_sho.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'My_Base_PostGre',
+'USER': 'postgres',
+'PASSWORD': '111',
+'HOST': '127.0.0.1',
+'PORT': '5432',
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
